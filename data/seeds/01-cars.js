03700e142a731d7d974bccf0e23da84785998a29
Sean Kramer <seandominickramer@gmail.com>
2021-02-26 17:21:57 -0500

    Add cars seeds

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,11 @@
 // STRETCH
+exports.seed = async function(knex) {
+  await knex('cars').truncate()
+  
+  await knex('cars').insert([
+    {vin: 'efrge44g', make: 'Saturn', model: 'Wagon', mileage: 30000, title: 'mine', transmission: 'good'},
+    {vin: '4gergersffs', make: 'Ford', model: 'Taurus', mileage: 444, title: 'yours', transmission: 'bad'},
+    {vin: 'srgg44t3', make: 'VW', model: 'Bug', mileage: 1.5},
+    {vin: 're5e5ye', make: 'Harley', model: 'Davidson', mileage: 67054},
+  ])
+}
