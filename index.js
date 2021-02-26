939a004a11a4d24d2f4c0c754002c76b7261a5bc
Sean Kramer <seandominickramer@gmail.com>
2021-02-26 17:10:37 -0500

    Add cars table schema

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ const server = require('./api/server')
 
 const port = process.env.PORT || 5000;
 
+server.get('/welcome', (req, res) => {
+  res.send('welcome to cars api')
+})
+
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`))
